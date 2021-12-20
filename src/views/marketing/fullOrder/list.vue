eacea40e4bcd1096b85569646b943d09e1f58492
hongbingkun <1810972990@qq.com>
2021-12-20 13:35:46 +0800

    fix:满件优惠列表活动状态修改

--- src/views/marketing/fullOrder/list.vue
+++ src/views/marketing/fullOrder/list.vue
@@ -124,9 +124,9 @@ export default {
   data () {
     return {
       couponStatus: [
+        { value: '0', label: '未开始' },
         { value: '1', label: '进行中' },
-        { value: '2', label: '已暂停' },
-        { value: '3', label: '已结束' }
+        { value: '2', label: '已结束' }
       ],
       useTypes: [
         { value: '1', label: '满件打折' },
